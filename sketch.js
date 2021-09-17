643f0203deec0de63ed271bff3380b2b18d47d25
Bilva726Patel <85816657+Bilva726Patel@users.noreply.github.com>
2021-09-17 14:45:14 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -58,6 +58,7 @@ function draw() {
   
     // Increase score if sword touching fruit
     if(fruitGroup.isTouching(knife)){
+      score= score+2
       fruitGroup.destroyEach();
       knifeSwooshSound.play();
     }
@@ -94,6 +95,7 @@ function draw() {
 function Monster(){
   if(World.frameCount%200===0){
     monster=createSprite(400,200,20,20);
+    monster.velocityX = -(8 + (score/10));
     monster.addAnimation("moving", monsterImage);
     monster.y=Math.round(random(100,550));
     //update below give line of code for increase monsterGroup speed by 10
